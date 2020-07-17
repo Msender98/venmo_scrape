231f8c9624f4128db352e87a504f0d382d5cd30b
Mike Sender <msender98@gmail.com>
2020-07-17 18:12:16 -0400

    skeleton data structures

--- my_scrape.py
+++ my_scrape.py
@@ -1,5 +1,7 @@
 from venmo_scrape import Venmo_Scrape_Client
+import pandas as pd
 import config
+
 
 '''
 Script to pull data from venmo's api and save it locally.
@@ -16,7 +18,25 @@ scrape = Venmo_Scrape_Client(access_token = access_token)
 
 my_user = scrape.user.get_my_profile()
 
-print(scrape.user_scrape(user = my_user))
+#example methods from Venmo_Scrape_Client
+# print(scrape.user_scrape(user = my_user))
+# transactions = scrape.user.get_user_transactions(user = my_user)
+# #print(transactions)
+# print(scrape.transaction_scrape(transactions[0]))
+
+
+#One dataframe to store users information
+
+
+user_df = pd.DataFrame(scrape.user_scrape(user = my_user), index = [0])
+
+
+#One dataframe to store transactions
+transaction_df = pd.DataFrame()
+
+print(user_df)
 
 scrape.log_out(f'Bearer {access_token}')
+
+
 
--- venmo_scrape.py
+++ venmo_scrape.py
@@ -13,21 +13,36 @@ class Venmo_Scrape_Client(Client):
 
     def user_scrape(self, user = None):
         '''
-        wdwd
+        Outputs user data from a venmo
         '''
         if not user:
             raise ValueError('user must be a venmo.user.User object')
 
         item = {}
         item['username'] = user.username
-        # item['username'] = user.username
-        # item['username'] = user.username
-        # item['username'] = user.username
-        # item['username'] = user.username
+        item['first_name'] = user.first_name
+        item['last_name'] = user.last_name
+        item['user_id'] = user.id
+        item['phone'] = user.phone
+        item['profile_picture_url'] = user.profile_picture_url
+        item['is_active'] = user.is_active
         return item
 
-    def transaction_scrape(self,user = None):
 
-        if not user:
-            raise ValueError('user must be a venmo.user.User object')
-        pass
+    def transaction_scrape(self, transaction = None):
+        '''
+        Outputs data from a venmo transaction object
+
+        '''
+
+
+        item = {}
+        item['actor_id'] = transaction.actor.id 
+        item['date_completed'] = transaction.date_completed 
+        item['date_created'] = transaction.date_created
+        item['note'] = transaction.note
+        item['target_id'] = transaction.target.id
+
+        return item
+
+
